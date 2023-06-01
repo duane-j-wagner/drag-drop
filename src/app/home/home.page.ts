eef12eca5c8b9fa0369b87d5a22d472c5b4f3487
Duane Wagner <duane.wagner@infor.com>
2023-06-01 14:34:47 -0500

    added cdk-scrollabe attribute with code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,16 @@
-import { Component, inject } from '@angular/core';
-import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop'
-import { RefresherCustomEvent } from '@ionic/angular';
+import {
+  AfterViewInit,
+  Component,
+  Renderer2,
+  ViewChild,
+  inject,
+} from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import {
+  DomController,
+  IonContent,
+  RefresherCustomEvent,
+} from '@ionic/angular';
 
 import { DataService, Message } from '../services/data.service';
 
@@ -9,9 +19,23 @@ import { DataService, Message } from '../services/data.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements AfterViewInit {
+  @ViewChild(IonContent)
+  ionContent: IonContent | undefined;
+  private scrollElement: HTMLElement | undefined;
+
+
   private data = inject(DataService);
+  private domController = inject(DomController);
+  private renderer = inject(Renderer2);
   constructor() {}
+
+  async ngAfterViewInit(): Promise<void> {
+    this.scrollElement = await this.ionContent?.getScrollElement();
+    this.domController.write(() => {
+      this.renderer.setAttribute(this.scrollElement, 'cdk-scrollable', '');
+    });
+  }
 
   refresh(ev: any) {
     setTimeout(() => {
@@ -24,7 +48,11 @@ export class HomePage {
   }
 
   drop(event: any) {
-    const ddEvent = event as CdkDragDrop<Message[]>
-    moveItemInArray(this.getMessages(), event.previousIndex, event.currentIndex);
+    const ddEvent = event as CdkDragDrop<Message[]>;
+    moveItemInArray(
+      this.getMessages(),
+      event.previousIndex,
+      event.currentIndex
+    );
   }
 }
